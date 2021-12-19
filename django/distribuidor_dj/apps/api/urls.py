0ea0b8e3265dea62952134ac28edb5b6d09865b2
Diego Sánchez <diegosandmg@gmail.com>
2021-12-19 15:36:13 -0400

    refactor: with linters info

--- django/distribuidor_dj/apps/api/urls.py
+++ django/distribuidor_dj/apps/api/urls.py
@@ -1,11 +1,22 @@
-from django.contrib import admin
-from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.urls import path
 
 urlpatterns = [
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
-    path('docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(
+        "docs/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
