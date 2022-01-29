b3d290861e634d769449088ed2e1be51f03d03fe
Diego Sánchez <diegosandmg@gmail.com>
2022-01-29 11:21:42 -0400

    feat: shipments view

--- django/distribuidor_dj/apps/dashboard/views.py
+++ django/distribuidor_dj/apps/dashboard/views.py
@@ -1,10 +1,19 @@
 """
 Dashboard views
 """
+from distribuidor_dj.apps.shipment.models import Shipment
+
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 
 from .mixins import DashboardPassesTestMixin
 
 
 class Index(DashboardPassesTestMixin, TemplateView):
     template_name = "dashboard/index.html"
+
+
+class ShipmentsView(DashboardPassesTestMixin, ListView):
+    template_name = "dashboard/shipments.html"
+    model = Shipment
+    paginate_by = 1  # change this later to 10
