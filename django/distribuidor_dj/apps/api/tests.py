767cdcb5845d123cdd659c5e15bcaaa7d6eb29d5
Diego Sánchez <diegosandmg@gmail.com>
2022-02-06 08:51:43 -0400

    create shipment testdata comment

--- django/distribuidor_dj/apps/api/tests.py
+++ django/distribuidor_dj/apps/api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase  # noqa F401
 
 # Create your tests here.
+"""
+{
+  "productquantity_set": [
+    {
+      "quantity": 1,
+      "name": "Zapatos Marca X"
+    }
+  ],
+  "target_address": {
+    "state": "Zulia",
+    "city": "Maracaibo",
+    "street": "Calle 23"
+  },
+  "commerce": "gshoes"
+}
+"""
