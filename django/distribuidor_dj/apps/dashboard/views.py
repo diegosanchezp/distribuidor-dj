209a41a25711c537a53884d5eed568f4f2390faf
Diego Sánchez <diegosandmg@gmail.com>
2022-02-09 21:17:05 -0400

    feat: filter shipments by state

--- django/distribuidor_dj/apps/dashboard/views.py
+++ django/distribuidor_dj/apps/dashboard/views.py
@@ -4,9 +4,11 @@ Dashboard views
 from distribuidor_dj.apps.invoice.models import Invoice
 from distribuidor_dj.apps.shipment.models import Shipment
 
+from django.db.models.query import QuerySet
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 
+from .forms import ShipmentsFilterForm
 from .mixins import DashboardPassesTestMixin
 
 
@@ -18,6 +20,27 @@ class ShipmentsView(DashboardPassesTestMixin, ListView):
     template_name = "dashboard/shipments.html"
     model = Shipment
     paginate_by = 1  # change this later to 10
+    extra_context = {"filter_form": ShipmentsFilterForm()}
+
+    def get_context_data(self):
+        ctx = super().get_context_data()
+        ctx["filter_form"] = ShipmentsFilterForm(data=self.request.GET)
+        return ctx
+
+    def get_queryset(self) -> "QuerySet[Shipment]":
+        queryset = super().get_queryset()
+        queryset = queryset.filter(commerce=self.request.user)
+        queryset = self.apply_search(queryset)
+        return queryset
+
+    def apply_search(
+        self, queryset: "QuerySet[Shipment]"
+    ) -> "QuerySet[Shipment]":
+        form = ShipmentsFilterForm(data=self.request.GET)
+        if form.is_valid():
+            state = form.cleaned_data["state"]
+            queryset = queryset.filter(state=state)
+        return queryset
 
 
 class InvoicesView(DashboardPassesTestMixin, ListView):
