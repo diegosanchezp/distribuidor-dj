684a31ddd6be86bcde296749f1ddad7c4990f8da
Diego Sánchez <diegosandmg@gmail.com>
2022-02-18 21:58:04 -0400

    refactor: events enum into models.TextChoice

--- django/distribuidor_dj/apps/invoice/models.py
+++ django/distribuidor_dj/apps/invoice/models.py
@@ -1,19 +1,12 @@
 import uuid
-from enum import auto, unique
 
 from distribuidor_dj.apps.state.models import StateMachineModel, StatusDate
 from distribuidor_dj.utils import const
-from distribuidor_dj.utils.enum import AutoName
 
 from django.apps import apps
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-
-@unique
-class InvoiceEvents(AutoName):
-    ON_PAY = auto()
 
 
 # Create your models here.
@@ -30,11 +23,18 @@ class Invoice(StateMachineModel):
         PAID = "PAID", _("Pagado")
         UNPAID = "UNPAID", _("No pagado")
 
+    class Events(models.TextChoices):
+        """
+        Invoice Events enumeration
+        """
+
+        ON_PAY = "ON_PAY", _("Pagar")
+
     status_date_relattr = "invoice"
 
     machine = {
         States.UNPAID: {
-            InvoiceEvents.ON_PAY: States.PAID,
+            Events.ON_PAY: States.PAID,
         }
     }
 
