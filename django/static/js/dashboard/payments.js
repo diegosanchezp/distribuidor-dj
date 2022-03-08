9b7d026d4a7df0d62e4a529cf53e3cdaf1a24491
Diego Sánchez <diegosandmg@gmail.com>
2022-03-08 19:41:17 -0400

    feat: finish ui interactivity

--- django/static/js/dashboard/payments.js
+++ django/static/js/dashboard/payments.js
@@ -1,22 +1,9 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  const bankSelectionForm = document.getElementById("select-banks-form");
-  const degvaForm = document.getElementById("degvaForm");
 
-  bankSelectionForm.addEventListener("submit", (e)=>{
-    // on form submission, prevent default
-    e.preventDefault();
-    const SelectionFormData = new FormData(bankSelectionForm);
-
-    const selectedBank = SelectionFormData.get("bank");
-
-    if(selectedBank === "degva"){
-      degvaForm.submit();
+document.addEventListener('alpine:init', () => {
+  Alpine.data('selectionForm', () => ({
+    currentForm: '',
+    dakitiSubmit(){
+      console.log("dakiti");
     }
-
-    if(selectedBank === "dakiti"){
-      // Probably do a fetch
-      console.log(selectedBank);
-    }
-  });
-
+  }));
 });
