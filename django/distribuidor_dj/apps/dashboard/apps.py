6437eced0f0632db62b72311036119e89f2205b2
Diego Sánchez <diegosandmg@gmail.com>
2022-03-16 16:29:02 -0400

    feat: run qcluster along django server

--- django/distribuidor_dj/apps/dashboard/apps.py
+++ django/distribuidor_dj/apps/dashboard/apps.py
@@ -1,6 +1,15 @@
+import os
+
 from django.apps import AppConfig
 
 
 class DashboardConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "distribuidor_dj.apps.dashboard"
+
+    def ready(self):
+        from django_q.cluster import Cluster
+
+        if os.environ.get("RUN_MAIN", None) != "true":
+            q = Cluster()
+            q.start()
