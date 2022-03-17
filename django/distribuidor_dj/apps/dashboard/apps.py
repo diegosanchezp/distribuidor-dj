d2308d83b31537c1ca05d79cac364e15c4f22ab2
Diego Sánchez <diegosandmg@gmail.com>
2022-03-16 21:34:14 -0400

    test-feat: heroku workes

--- django/distribuidor_dj/apps/dashboard/apps.py
+++ django/distribuidor_dj/apps/dashboard/apps.py
@@ -1,18 +1,6 @@
-import os
-
 from django.apps import AppConfig
 
 
 class DashboardConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "distribuidor_dj.apps.dashboard"
-
-    def ready(self):
-        from django_q.cluster import Cluster
-
-        if (
-            os.environ.get("RUN_MAIN", None) != "true"
-            and not os.environ.get("BUILDING", False) == "true"
-        ):
-            q = Cluster()
-            q.start()
