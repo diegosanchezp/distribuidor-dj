481091e6729545e7c0e20675bc5e6aad8f8796b4
Diego Sánchez <diegosandmg@gmail.com>
2022-03-17 15:40:49 -0400

    fix: working with invoices instead of invoice status dates

--- django/distribuidor_dj/bg_tasks.py
+++ django/distribuidor_dj/bg_tasks.py
@@ -5,7 +5,7 @@ Background tasks
 import datetime
 
 # Write here functions you want to schedule.
-from distribuidor_dj.apps.invoice.models import Invoice
+from distribuidor_dj.apps.invoice.models import Invoice, InvoiceStatusDate
 
 from django.db.models import DurationField, ExpressionWrapper, F, Q
 from django.utils import timezone
@@ -14,18 +14,19 @@ from django.utils import timezone
 def check_invoices():
     """
     Check for due invoices, does the check once an hour
+    Runs if the django_q cluster is activated.
     """
     # Get invoices that are not overdue
     # Check invoices that might be overdue:
     # time elapsed from creation date to todays date
     # (date difference) is greater than 15 day
     # Reference https://stackoverflow.com/a/50601209
-    overdue_invoices = (
-        Invoice.objects.filter(~Q(dates__status=Invoice.States.OVERDUE))
+    overdue_invoices_dates = (
+        InvoiceStatusDate.objects.filter(Q(status=Invoice.States.UNPAID))
         .annotate(
             # date_diff type is timedelta
             date_diff=ExpressionWrapper(
-                timezone.now() - F("dates__date"), output_field=DurationField()
+                timezone.now() - F("date"), output_field=DurationField()
             )
         )
         .filter(date_diff__gte=datetime.timedelta(days=15))
@@ -34,17 +35,13 @@ def check_invoices():
     # invoice_dates = []
 
     # Set the OVERDUE status dates.
-    for invoice in overdue_invoices:
-        inv_sd = invoice.transition_set_date(Invoice.Events.ON_OVERDUE)
-        invoice.save()
+    for overdue_dt in overdue_invoices_dates:
+        inv_sd = overdue_dt.invoice.transition_set_date(
+            Invoice.Events.ON_OVERDUE
+        )
+        overdue_dt.invoice.save()
         inv_sd.save()
 
     #     overdue_invoices.update(state=Invoice.States.OVERDUE)
     #
     #     InvoiceStatusDate.objects.bulk_create(invoice_dates)
-
-    """
-    invoice -> 15/1/2022
-    todays_date 30/1/2022
-    todays_date - invoice >= 15 ? Yes
-    """
