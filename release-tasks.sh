f59e777d01d6268114e334a1ecfbfe98644aa380
Diego Sánchez <diegosandmg@gmail.com>
2022-03-17 21:55:59 -0400

    fix: loaddata command

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,2 +1,2 @@
 python django/manage.py migrate --noinput --settings distribuidor_dj.settings.production
-python django/manage.py --settings distribuidor_dj.settings.production loaddata django/fixtures/{address-state.json,address.json,bg_task.json}
+python django/manage.py loaddata --settings distribuidor_dj.settings.production django/fixtures/{address-state.json,address.json,bg_task.json}
